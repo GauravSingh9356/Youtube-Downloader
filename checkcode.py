769abba313283bd98459af190e60bca16cd2ae89
GauravSingh75 <55052983+GauravSingh9356@users.noreply.github.com>
2020-07-14 15:48:24 +0530

    Version 1.0.0

---
+++ checkcode.py
@@ -0,0 +1,19 @@
+# URL="https://www.youtube.com/watch?v=f8YuMLSONFU"
+# path_save="C:\\Users\\gs935\\Desktop"
+
+# strms=ob.streams.all()
+# for s in strms:
+# print(s)
+
+# print(ob.title)
+#print(ob.author, ob.captions, ob.caption_tracks,ob.length,ob.vid_info,ob.views)
+# print(strm.filesize, strm.title, ob.description)
+
+#text = Text(main)
+#text.insert(CENTER, title)
+#text.tag_configure("center", justify='center')
+# text.pack(side=TOP)
+# print(ob.description, ob.title)
+
+# label.configure(anchor="center")
+# label.pack(side=TOP)
